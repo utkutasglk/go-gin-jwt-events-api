1b4ce44225712481507361773c772d2dd170e375
utkutasglk <152283834+utkutasglk@users.noreply.github.com>
2025-06-24 00:13:48 +0300

    create database tables and project structure

---
+++ cmd/migrate/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users(
+    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL
+);
---
+++ go.mod
@@ -0,0 +1,8 @@
+module rest-go-gin
+
+go 1.24.2
+
+require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+)
